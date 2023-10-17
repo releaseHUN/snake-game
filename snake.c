b0368cc96bc1d066369d5a0d54b616d8db77d33d
unknown <vasember@msn.com>
2023-10-17 18:25:32 +0200

    some snake functions

--- snake.c
+++ snake.c
@@ -1,7 +1,33 @@
 #include "snake.h"
+#include <stdlib.h>
 
-typedef struct snakeType {
-	int x;
-	int y;
-	int direction;
-} snakeType;
+snakeType *init(int x, int y){
+	snakeType *stSnake = malloc(sizeof(snakeType));
+	stSnake->x = x;
+	stSnake->y = y;
+	stSnake->next = NULL;
+	return stSnake;
+}
+
+void addSegment(snakeType *stSnake) {
+	snakeType *stTemp = malloc(sizeof(snakeType));
+	while(stSnake->next != NULL)
+		stSnake = stSnake->next;
+	stSnake->next = stTemp;
+	stSnake->next->x = stSnake->x - stSnake->directionX;
+	stSnake->next->y = stSnake->y - stSnake->directionY;
+	stSnake->next->directionX = stSnake->directionX;
+	stSnake->next->directionY = stSnake->directionY;
+}
+
+void moveSnake(snakeType *stSnake, int directionX, int directionY) {
+	stSnake->directionX = directionX;
+	stSnake->directionY = directionY;
+	while(stSnake->next != NULL){
+		stSnake->x += stSnake->directionX;
+		stSnake->y += stSnake->directionY;
+		stSnake->next->directionX = stSnake->directionX;
+		stSnake->next->directionY = stSnake->directionY;
+		stSnake = stSnake->next;
+	}
+}
--- snake.h
+++ snake.h
@@ -1,4 +1,21 @@
 #ifndef SNAKE_GAME_SNAKE_H
 #define SNAKE_GAME_SNAKE_H
 
+typedef struct snakeType {
+	int x;
+	int y;
+	int directionX;
+	int directionY;
+	struct snakeType *next;
+} snakeType;
+
+//inicializes a snake at the given coordinates
+snakeType *init(int x, int y);
+
+//adds a new segment to the snake
+void addSegment(snakeType *stSnake);
+
+//moves the snake forward in the given direction
+void moveSnake(snakeType *stSnake, int directionX, int directionY);
+
 #endif
